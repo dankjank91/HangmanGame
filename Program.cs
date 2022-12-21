df63d930d487d8dbee8ecd172f22d96364a22fbe
christianp <cp85208520@gmail.com>
2022-12-21 01:22:12 -0700

    Working version

--- Program.cs
+++ Program.cs
@@ -12,19 +12,56 @@ namespace Main
         static void Main(string[] args)
         {
             Random random = new Random();
-            string[] words = { "Nintendo","Sony","Sega","Microsoft" };
+            string[] words = { "Program","Code","String","Variable","Integer","Character","Array","Loop","Object","Object-Oriented-Programming" };
             int randomNumber = random.Next(words.Length);
             string beforeWord = words[randomNumber];
             string afterWord = beforeWord;
+            string inputWord = afterWord;
             char hider = '*';
+            bool unmatched = true;
+            bool play = false;
 
             for (int i = 0; i < afterWord.Length; i++)
             {
-                afterWord = afterWord.Replace(afterWord[i], hider);
-
+                afterWord = afterWord.Remove(i,1);
+                afterWord = afterWord.Insert(i,hider.ToString());
             }
-            Console.WriteLine("Before the word was encrypted it was {0}. Then, after it was encrypted it was {1}.",beforeWord,afterWord);
-            Console.WriteLine("Before the word was encrypted it was {0} letters. Then, after it was encrypted it was {1} letters.",beforeWord.Length,afterWord.Length);
+            Console.WriteLine("HANGMAN");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Can you save him from the gallows?");
+            Console.WriteLine("y or n");
+            do
+            {
+
+
+                char yesNo = char.Parse(Console.ReadLine());
+                if (yesNo == 'y') { play = true; Console.Beep(); } else if (yesNo == 'n') { play = false; Console.Beep(); }
+            }
+            while (!play);
+            do
+            {
+                Console.WriteLine("<>{0}<>", afterWord);
+
+                Console.WriteLine("See if you can guess the word, enter a letter now");
+                char guess = char.Parse(Console.ReadLine()); Console.Clear(); 
+                for (int i = 0; i < afterWord.Length; i++)
+                {
+                    if (beforeWord[i] == guess) 
+                    { 
+                        afterWord= afterWord.Remove(i,1); 
+                        afterWord = afterWord.Insert(i, guess.ToString());
+                        Console.Beep();
+                    }
+                   if (!afterWord.Contains('*')) { unmatched = false; }
+
+                }
+            } while (unmatched);
+
+            if (!unmatched) { Console.WriteLine("You got it, the word was {0}", afterWord); }
+            else { Console.WriteLine("See ya later"); }
+            
+
         }
     
     }
